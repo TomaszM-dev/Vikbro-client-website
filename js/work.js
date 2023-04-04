51556c82d1ddf4d28e8c80aaa27340bcc1633f52
TomaszM-dev <tomekmalocha@gmail.com>
2023-04-04 02:45:35 +0200

    work section done, couple of animations and responsivness left

--- js/work.js
+++ js/work.js
@@ -11,28 +11,125 @@ const compositionBtnRight = document.querySelector(".composition__btn-right");
 const content = document.querySelectorAll(".content__left");
 const compositionBntReturn = document.querySelector(".composition__btn-return");
 const compositionContent = document.querySelector(".content__headline");
+const compositionImage = document.querySelectorAll(".composition__image");
+
+//  query selectors that will be dependent upon choossed project
+const headingH2 = document.querySelector(".popup__content-heading");
+const contentParagraph0 = document.querySelector(".content__paragraph-box0");
+const contentParagraph1 = document.querySelector(".content__paragraph-box1");
+const contentParagraph2 = document.querySelector(".content__paragraph-box2");
+const contentParagraph3 = document.querySelector(".content__paragraph-box3");
+const contentHeadline0 = document.querySelector(".content__headline-box0");
+const contentHeadline1 = document.querySelector(".content__headline-box1");
+const contentHeadline2 = document.querySelector(".content__headline-box2");
+const contentHeadline3 = document.querySelector(".content__headline-box3");
+const compositionImage1 = document.querySelector(".image-1");
+const compositionImage2 = document.querySelector(".image-2");
+const compositionImage3 = document.querySelector(".image-3");
 
 export const workFunction = function () {
-  // pierwszy slide
-  let curSlide = 1;
-  // max slidow
-  const maxSlide = 3;
+  const altana = {
+    position: "1",
+    heading: "Altana",
+    headline: "project altanka",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "./img/work/altana1.jpeg",
+    photo2: "./img/work/altana2.jpeg",
+    photo3: "./img/work/altana3.jpeg",
+  };
 
-  // otwarcie popupa
-  const initState = function () {
-    popup.classList.remove("popup__close");
-    popupContent.classList.remove("scale__open");
+  const dom = {
+    position: "2",
+    heading: "Dom",
+    headline: "project dom",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "/img/work/dom1.jpeg",
+    photo2: "/img/work/dom2.jpeg",
+    photo3: "/img/work/dom3.jpeg",
+  };
+
+  const garazParking = {
+    position: "3",
+    heading: "garaz",
+    headline: "project garaz",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "/img/work/garaz1.jpeg",
+    photo2: "/img/work/garaz2.jpeg",
+    photo3: "/img/work/garaz3.jpeg",
+  };
+  const domek = {
+    position: "4",
+    heading: "Domek",
+    headline: "project domek",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "/img/work/garazyk1.jpeg",
+    photo2: "/img/work/garazyk2.jpeg",
+    photo3: "/img/work/garazyk3.jpeg",
+  };
+  const taras = {
+    position: "5",
+    heading: "taras",
+    headline: "project taras",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "/img/work/taras1.jpeg",
+    photo2: "/img/work/taras2.jpeg",
+    photo3: "/img/work/taras3.jpeg",
+  };
+  const dom2 = {
+    position: "6",
+    heading: "Dom2",
+    headline: "project dom2",
+    paragraph: ` TU BEDZIE ZNAJDOWAL SIE GLOWNY OPIS CO TO ZA PROJEKT ITP ITD,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum,`,
+    photo1: "/img/work/hause1.jpeg",
+    photo2: "/img/work/hause2.jpeg",
+    photo3: "/img/work/hause3.png",
+  };
+
+  const projects = [altana, dom, garazParking, domek, taras, dom2];
+
+  let curSlide = 1;
+  const maxSlide = 3;
+  let currentProject;
+
+  const openPopup = function () {
+    popup.classList.add("popup__open");
+    popupContent.classList.add("scale__open");
+
     popupContent.classList.remove("scale__close");
+    popup.classList.remove("popup__close");
   };
 
   itemsContainer.addEventListener("click", function (e) {
     e.preventDefault();
+    const clicked = e.target.closest(".item");
+    if (!clicked) return;
 
-    initState();
+    currentProject = projects.find(
+      (pro) => pro.position === clicked.dataset.value
+    );
 
-    // otwarcie itemu za posrednictwiem klikniecia w btn na worku
-    popup.classList.add("popup__open");
-    popupContent.classList.add("scale__open");
+    if (currentProject.position === clicked.dataset.value) {
+      headingH2.textContent = currentProject.heading;
+      contentParagraph0.textContent = currentProject.paragraph;
+      contentParagraph1.textContent = currentProject.paragraph;
+      contentParagraph2.textContent = currentProject.paragraph;
+      contentParagraph3.textContent = currentProject.paragraph;
+      contentHeadline0.textContent = currentProject.headline;
+      contentHeadline1.textContent = currentProject.headline;
+      contentHeadline2.textContent = currentProject.headline;
+      contentHeadline3.textContent = currentProject.headline;
+      compositionImage1.src = currentProject.photo1;
+      compositionImage2.src = currentProject.photo2;
+      compositionImage3.src = currentProject.photo3;
+
+      openPopup();
+    }
   });
 
   // powrot do poczatkowego ustawienia
@@ -155,6 +252,10 @@ export const workFunction = function () {
     if (!clicked) return;
 
     curSlide = Number(clicked.dataset.set);
+
+    compositionImage.forEach((photo) => {
+      console.log(photo.getAttribute("src"));
+    });
 
     // 1. DODAC DISPLY NONE DO STARTERA
     contentStarterBox.classList.add("composition__display");
